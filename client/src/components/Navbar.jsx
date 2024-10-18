6d4fa3a314e9c66e5e301b27d80a7fd7c1553827
wilson mwangi waweru <kouragitam@gmail.com>
2024-10-18 12:01:21 +0300

    modifies

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Make sure to install react-router-dom
+import { Link } from 'react-router-dom'; // Ensure you have react-router-dom installed
+import { FaShoppingCart } from 'react-icons/fa'; // Importing a cart icon from react-icons (ensure you have installed react-icons)
 
 const Navbar = () => {
     return (
@@ -9,7 +10,7 @@ const Navbar = () => {
             </div>
             <ul style={styles.navLinks}>
                 <li style={styles.navItem}>
-                    <Link to="/" style={styles.link}>Login</Link>
+                    <Link to="/" style={styles.link}>Home</Link>
                 </li>
                 <li style={styles.navItem}>
                     <Link to="/signup" style={styles.link}>Signup</Link>
@@ -19,6 +20,13 @@ const Navbar = () => {
                 </li>
                 <li style={styles.navItem}>
                     <Link to="/about" style={styles.link}>About</Link>
+                </li>
+                {/* Adding Cart link */}
+                <li style={styles.navItem}>
+                    <Link to="/cart" style={styles.cartLink}>
+                        <FaShoppingCart size={20} />
+                        {/* You can also display the cart count next to the cart icon */}
+                    </Link>
                 </li>
             </ul>
         </nav>
@@ -50,6 +58,13 @@ const styles = {
         color: '#fff',
         textDecoration: 'none',
         fontSize: '18px',
+    },
+    cartLink: {
+        color: '#fff',
+        textDecoration: 'none',
+        fontSize: '18px',
+        display: 'flex',
+        alignItems: 'center',
     },
 };
 
